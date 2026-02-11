5404d2d45f8a59ca25bc53854be90d8cea111c30
Sonia Ngaruiya <soniangaruiya@gmail.com>
2026-02-11 15:10:44 +0300

    Render list of places

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ let places = []
 function renderEmptyState(){
   return`
   <div class="emptystate">
-  <h2> Glimpse of memories</h2>`
+  <h2> Glimpse of memories</h2>
   <p> Add a new location to get started </p>
   </div>`;
 }
+
+function renderPlacesList(){
+  const output = document.getElementById("placedisplay");
+  output.innerHTML = "";
+
+  if(places.length === 0){
+    output.innerHTML = renderEmptyState();
+    return;
+  }
+
+  
+}
+
+
